e521d50e753f6624bdb595dab0dba17b197f4f6b
Beliars <2beliars@gmail.com>
2016-07-03 19:44:36 +0300

    clock stopwatch added

--- homeworks/js/clock/js/script.js
+++ homeworks/js/clock/js/script.js
@@ -1,26 +1,44 @@
 var timeElem = $("#time"), 
 	timeStart = $("#start"), 
-	timeReset = $("#reset");
-
-function startTime() { 
-	var interval = setInterval(function() {
-		var date = new Date();
-		var hours = date.getHours(), minutes = date.getMinutes(), seconds = date.getSeconds();
-		if (hours < 10) hours = "0" + hours;
-		if (minutes < 10) minutes = "0" + minutes;
-		if (seconds < 10) seconds = "0" + seconds;
-		timeElem.html(hours + ":" + minutes + ":" + seconds);
-	}, 1000);
-	timeReset.on("click", function() {
-		return resetTime(interval);
-	});
-}
-
-function resetTime(interval) {
-	clearInterval(interval);
-	timeElem.html("00:00:00");
-}
+	timeReset = $("#reset"),
+	stopwatchStart = $("#startStopwatch"),
+	stopwatchStop = $("#stopStopwatch"),
+	interval,
+	seconds = 0;
 
 timeStart.on("click", function() {
-	return startTime();
+	clearInterval(interval);
+	startTime();
+	interval = setInterval(startTime, 1000);
 });
+
+timeReset.on("click", function() {
+	clearInterval(interval);
+	seconds = 0;
+	timeElem.html("00:00:00");
+});
+
+stopwatchStart.on("click", function() {
+	clearInterval(interval);
+	startStopwatch();
+	interval = setInterval(startStopwatch, 1000);
+});
+
+stopwatchStop.on("click", function() {
+	clearInterval(interval);
+});
+
+function startTime() { 
+	var date = new Date();
+	var hours = date.getHours(), minutes = date.getMinutes(), seconds = date.getSeconds();
+	if (hours < 10) hours = "0" + hours;
+	if (minutes < 10) minutes = "0" + minutes;
+	if (seconds < 10) seconds = "0" + seconds;
+	timeElem.html(hours + ":" + minutes + ":" + seconds);
+}
+
+function startStopwatch() {
+	if (seconds < 10) timeElem.html("00:00:0" + seconds);
+	else timeElem.html("00:00:" + seconds);
+	seconds++;
+}
